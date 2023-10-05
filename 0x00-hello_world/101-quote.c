e8eedd94d17b88aa242cff0b12cd75d783d00fbb
mikelexx <michael.muri2020@gmail.com>
2023-10-05 12:47:43 -0700

    modified type of funtion to print error message

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,5 +1,4 @@
-#include <stdio.h>
-#include <errno.h>
+#include <unistd.h>
 /**
  * main - entry point.
  * Description - print error message without using puts or printf their man 3.
@@ -7,5 +6,7 @@
  */
 int main(void)
 {
-perror("and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+const char *message = "and that piece of art is useful\" - Dora Korpar, 2015-10-1";
+write(2,message,strlen(message));
+return (1);
 }
