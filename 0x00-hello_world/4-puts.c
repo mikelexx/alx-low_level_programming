20e3b00f876ee5fe91b27e6ab3a5334894bf0142
mikelexx <michael.muri2020@gmail.com>
2023-10-05 08:27:09 -0700

    modified 4-puts.c file

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /*
- * main - entry point
+ *main - entry point
  *Return - always zero (for sucess)
 */
-int main (void)
+int main(void)
 {
-puts ("\"Programming is like building a multilingual puzzle");
+puts("\"Programming is like building a multilingual puzzle");
 return(0);
 }
