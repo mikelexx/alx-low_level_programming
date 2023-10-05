76ddd7bf5c5cce70f157e0e15e081c1b279701c2
mikelexx <michael.muri2020@gmail.com>
2023-10-05 08:31:42 -0700

    modified file

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,7 @@
 #include <stdio.h>
-/*
- *main - entry point
- *Return - always zero (for sucess)
-*/
+/*prints string and returns sucess*/
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
-return(0);
+return (0);
 }
