bb14e4f08facf8abac97e8c536e8a16f029adc91
mikelexx <michael.muri2020@gmail.com>
2023-10-05 14:19:56 -0700

    fixed a typo in in variables value

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,7 +7,7 @@
  */
 int main(void)
 {
-const char *message = "and that piece of art is useful\" - Dora Korpar, 2015-10-1";
+const char *message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
 write(2,message,59);
 return (1);
 }
