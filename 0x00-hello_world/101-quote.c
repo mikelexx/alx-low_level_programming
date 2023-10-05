6666cb83b0caacb997d575636defe0f2c82b2349
mikelexx <michael.muri2020@gmail.com>
2023-10-05 14:38:41 -0700

    removed trailing white spaces

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,11 +3,12 @@
 /**
  * main - entry point.
  * Description - print error message without using puts or printf their man 3.
- * Return: 1 for error. 
+ * Return: 1 for error.
  */
 int main(void)
 {
-const char *message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+const char *message = "and that piece of art is useful\" - Dora Korpar,
+      2015-10-19\n";
 write(2,message,59);
 return (1);
 }
