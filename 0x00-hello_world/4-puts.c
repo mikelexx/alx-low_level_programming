ec71ab45391c636f20a7b0a191585205fec4694b
mikelexx <michael.muri2020@gmail.com>
2023-10-05 08:35:08 -0700

    modified file

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-/*prints string and returns sucess*/
+/*Description: Prints string and returns sucess*/
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
