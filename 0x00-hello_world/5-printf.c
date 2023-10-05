be79fd77835cce6b5e0af5cfa48a98d4d0ed962b
mikelexx <michael.muri2020@gmail.com>
2023-10-05 08:58:47 -0700

    added new file

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - entrypoint.
+ * Description - prints a string about grammar in programming
+ * Return:zero always*/
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
