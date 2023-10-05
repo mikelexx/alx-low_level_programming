dfb4342abde61e7a92298f1fcdef69bd18732a43
mikelexx <michael.muri2020@gmail.com>
2023-10-05 11:26:20 -0700

    new file

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - entry point 
+ * Description - print error message without using puts or printf their man 3
+ * Return  - 1 for error 
+ */
+int main(void)
+{
+perror("and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+return (1);
+}
