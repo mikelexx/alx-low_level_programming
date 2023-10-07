fb5f8390c0701d38eabb268f7e140114fc28dc75
mikelexx <michael.muri2020@gmail.com>
2023-10-07 00:45:37 -0700

    added new file

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - entry pioint.
+ * Description - print all alphabets in lower case.
+ * Return: 0 always for success.
+ */
+int main(void)
+{
+	int letter = 'a';
+
+	while (letter <= 'z' )
+	{
+		putchar(letter);
+		letter++;
+	}
+	return (0);
+}
