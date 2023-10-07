7c52d358b73a531a069ff3ddb3f7e6d18786f278
mikelexx <michael.muri2020@gmail.com>
2023-10-07 01:47:19 -0700

    fixec betty style

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
 /**
- * mletterin -entry point .
- * Description - printletter letterll lowercletterse letters except q letternd e.
+ * main - entry point .
+ * Description - printletter letterll  \
+ * lowercletterse letters except q letternd e.
  * Return: 0 for success.
  */
 int main(void)
 {
 	int letter = 'a';
-	
+
 	 while (letter <= 'z')
 	 {
 		 if (letter != 'q' && letter != 'e')
