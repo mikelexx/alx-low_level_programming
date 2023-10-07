f298ff0da052f85c7a99685b4ab9d25ea3083d80
mikelexx <michael.muri2020@gmail.com>
2023-10-07 00:07:58 -0700

    add: added more logic statements ot 1-last_igit.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -14,6 +14,18 @@ int mai(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	last = n % 10;
-	printf("Last digit of %d is %d", n, last);
+	printf("Last digit of %d is ",last);
+	if (last > 5)
+	{
+		printf("and is greater than 5");
+	}
+	if (last == 0)
+	{
+		printf("and is 0");
+	}
+	if (last < 6 && last != 0)
+	{
+		printf("and is less than 6 and not 0");
+	}
 	return (0);
 }
