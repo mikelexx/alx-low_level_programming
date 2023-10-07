fd8b3cc119d037ea7c841b7b32ce85c9f11210bd
mikelexx <michael.muri2020@gmail.com>
2023-10-07 02:38:19 -0700

    fixed conditional statement

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -2,13 +2,13 @@
 /**
  * main - entry point.
  * Description  - print all base to numbers from 0 to 10.
- * Return : 0  to indicate sucess.
+ * Return: 0  to indicate sucess.
  */
 int main(void)
 {
 	int num = 0;
 
-	while (num <= 10)
+	while (num <= 9)
 	{
 		printf("%d", num);
 		num++;
