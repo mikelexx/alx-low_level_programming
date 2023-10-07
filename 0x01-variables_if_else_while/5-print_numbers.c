ea15ecda809a66d0345c1e25fe29f2cc1574f805
mikelexx <michael.muri2020@gmail.com>
2023-10-07 02:27:52 -0700

    new file added

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main -entry point.
+ * Description  - print all base to numbers from 0 to 10.
+ * Return : 0  to indicate sucess.
+ */
+int main(void)
+{
+	int num = 0;
+
+	while (num <= 10)
+	{
+		printf("%d",num);
+		num++;
+	}
+	putchar('\n');
+}
