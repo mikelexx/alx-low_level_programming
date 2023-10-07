a3bec4a045164780c89c9a5607338365b4cbce70
mikelexx <michael.muri2020@gmail.com>
2023-10-07 03:49:32 -0700

    modified file

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -2,18 +2,20 @@
 /**
  * main - entry ponit.
  * Description - prints all combinatios of single digits.
- * Return: 0 
+ * Return: 0.
  */
 int main(void)
 {
 	int num  = 48;
+
 	while (num < 56)
 	{
 		putchar(num);
 		putchar(44);
 		putchar(32);
+		num++;
 	}
 	putchar(57);
-	return(0);
+	return (0);
 }
 
