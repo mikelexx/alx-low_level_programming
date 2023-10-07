2f2cb8490b9d9840ba928ecb62500e3e81ca0628
mikelexx <michael.muri2020@gmail.com>
2023-10-07 01:03:40 -0700

    new file added

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - entry point .
+ * Description - print all uppercase and lowercase alphabets.
+ * Return: 0 for success.
+ */
+int main(void)
+{
+	int letter = 'a'
+	
+	while (letter <= 'z')
+	{
+		putchar(letter);
+		letter++;
+	}
+	letter = 'A';
+	while (letter <= 'Z')
+	{
+		puchar(letter);
+		letter++;
+	}
+	putchar('\n');
+	return (0);
