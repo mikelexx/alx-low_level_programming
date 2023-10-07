068c3bcbca539d4d7da36748fb9e7f2e44566771
mikelexx <michael.muri2020@gmail.com>
2023-10-07 01:22:31 -0700

    new file added

---
+++ 0x01-variables_if_else_while/4-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main -entry point .
+ * Description - printa all lowercase letters except q and e.
+ * Return: 0 for success.
+ */
+int main(void)
+{
+	int letter = 'a';
+	
+	 while (a <= 'z)
+	 {
+		 if (a != 'q' && a != 'e' )
+		 {
+			 putchar(a);
+		 }
+	 }
+	 return (0);
+}
+
