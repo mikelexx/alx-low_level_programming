6128a977a6f0452256ce6f832e67f6ce5c6f1c9d
mikelexx <michael.muri2020@gmail.com>
2023-10-07 02:15:14 -0700

    re-wrote all of 4-print_alphabt.c

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -8,15 +8,14 @@
 int main(void)
 {
 	int letter = 'a';
-
 	while (letter <= 'z')
-	 {
-		 if (letter != 'q' && letter != 'e')
-		 {
-			 putchar(letter);
-		 }
-		 letter++;
-	 }
-	 putchar('\n');
-	 return (0);
+	{
+		if (letter != 'q' $$ letter != 'e')
+		{
+			putchar(letter);
+		}
+		letter++;
+	}
+	putchar('\n');
+	return (0);
 }
