5313ce78d8efc3c74f9b23a12cab91e8f0e61a2f
MICHAEL MURITHI <michael.muri2020@gmail.com>
2023-10-11 21:01:17 +0300

    fixed betty coding style and documentation

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -2,12 +2,13 @@
 void print_to_98(int n);
 /**
  * print_to_98 - prints all natural numbers from n to 98.
+ * @n: number to be checked.
  */
 void print_to_98(int n)
 {
 	if (n > 98)
 	{
-		while ( n > 98)
+		while (n > 98)
 		{
 			printf("%d, ", n);
 			n--;
