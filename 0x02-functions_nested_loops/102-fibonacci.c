535dbadf0b6e62fe3f72406e46573a72fc680912
MICHAEL MURITHI <michael.muri2020@gmail.com>
2023-10-12 01:34:07 +0300

    new files added

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - entry point.
+ * Description - prints 50 fibonacci numbers.
+ * Return: 0 for success.
+ */
+int main(void)
+{
+	int prev = 1;
+	int curr = 2:
+	int i = 0
+
+	while (i <=48)
+	{
+		int temp = curr;
+		curr = curr + prev;
+		prev = temp;
+		printf("%d, ", curr);
+		i++;
+	}
+	return (0);
+}
+
