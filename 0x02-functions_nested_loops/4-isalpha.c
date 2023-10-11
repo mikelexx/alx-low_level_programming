114240443d02d354b15c74c07bda58ea6d49d7a3
mikelexx <michael.muri2020@gmail.com>
2023-10-11 06:19:42 -0700

    fixed betty style,documentation and relational statement error

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,11 +1,16 @@
 #include <stdio.h>
 int _isalpha(int c);
-
+/**
+ * _isalpha - checks for alphabetic character.
+ * @c: ascii integer to be checked.
+ *
+ * Return: 1 if its a letter 0 otherwise.
+ */
 int _isalpha(int c)
 {
-	if ( 65 <= c <= 90 || 97 <= c <= 122)
+	if ( c >= 65 && c <= 90 || c >= 97 && c <= 122)
 	{
-		return 1;
+		return (1);
 	}
-	return 0;
+	return (0);
 }
