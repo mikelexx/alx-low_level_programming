c5b2fca1faa480ce413171414fe5b893522da79f
mikelexx <michael.muri2020@gmail.com>
2023-10-11 06:36:17 -0700

    replaced printf with _putchar

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -10,14 +10,14 @@ int print_sign(int n)
 {
 	if (n > 0)
 	{
-		printf("1");
+		_putchar(43);
 		return (1);
 	}
 	if (n == 0)
 	{
-		printf("0");
+		_putchar(48);
 		return (0);
 	}
-	printf("-1");
+	_putchar(45);
 	return (-1);
 }
