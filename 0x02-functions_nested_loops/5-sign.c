fd73ff9451108523e77ac53737294fc32ee12004
mikelexx <michael.muri2020@gmail.com>
2023-10-11 06:26:15 -0700

    fixed betty style

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -2,6 +2,7 @@
 int print_sign(int n);
 /**
  * print_sign -  prints the sign of a number.
+ * @n: number to be checked.
  *
  * Return: 1 if n is greater than zero, 0 if n is zero else -1.
  */
@@ -10,7 +11,7 @@ int print_sign(int n)
 	if (n > 0)
 	{
 		printf("1");
-		return 1;
+		return (1);
 	}
 	if (n == 0)
 	{
