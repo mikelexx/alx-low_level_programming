10455348e8859eafd658f5bc202e91e28b393275
MICHAEL MURITHI <michael.muri2020@gmail.com>
2023-10-11 21:17:26 +0300

    new file added

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,27 @@
+#include "main.h"
+void times_table(void);
+/**
+ * times_table - prints the 9 times table starting with 0.
+ */
+void times_table(void)
+{
+	int row = 0;
+	int column = 0;
+	
+	while (row < 10)
+	{
+		column = 0;
+		int pcolumn;
+		while (column < 9)
+		{
+			pcolumn = column + row + 48;
+			_putchar(pcolumn);
+			_putchar(44);
+			_putchar(32);
+			column++;
+		}
+		int fcol = pcolumn + row;
+	       	putchar(fcol);
+       		row++;
+	}
+}	
