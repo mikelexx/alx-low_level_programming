21db0c9cc921c0f4214255a732539d40be63ae24
MICHAEL MURITHI <michael.muri2020@gmail.com>
2023-10-12 19:54:02 +0300

    new file added

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+/**
+ * main- performs fizz-buzz-test.
+ * Return: 0 always.
+ */
+void main(void)
+{
+	int num = 2;
+	printf(" %d",1);
+	while (num < 101)
+	{
+		if ((num % 3) == 0 && (num % 5) == 0)
+		{
+			printf(" FizzBuzz");
+		}
+		else if (num % 3 == 0)
+		{
+			printf(" Fizz");
+		}
+		else if (num % 5 == 0)
+		{
+			printf(" Buzz");
+		}
+		else
+		{
+			printf(" %d", num);
+		}
+		num++;
+	}
+}
