179034cb1c15c99aa96b66b7be4fb65aea30bf78
MICHAEL MURITHI <michael.muri2020@gmail.com>
2023-10-12 21:54:37 +0300

    included edge cases

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -7,22 +7,27 @@ void print_triangle(int size)
 {
 	int row = 1;
 	int col = 1;
+	if (size <= 0)
+	{
+		_putchar(10);
+		return;
+	}
 	while (row <= size)
 	{
 		col = 1;
 		while (col <= size)
 		{
-			if(col <= size-row)
+			if(col <= size - row)
 			{
 				_putchar(32);
 			}
-			else 
+			else
 			{
 				_putchar(35);
 				col++;
 			}
 		}
-		_putchar('\n');
+		_putchar(10);
 		row++;
 	}
 }
