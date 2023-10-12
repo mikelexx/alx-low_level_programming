7287ff571e2a1b41acd4d116011db5109870aca5
MICHAEL MURITHI <michael.muri2020@gmail.com>
2023-10-12 19:07:36 +0300

    newfile added

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * print_line - draws a straight line in the terminal.
+ * @n: length of the line to be printed.
+ */
+void print_line(int n)
+{
+	int curr = 0;
+	
+	if (n <= 0)
+	{
+		_putchar(10);
+		return;
+	}
+	while (curr < n)
+	{
+		_putchar(95);
+		curr++;
+	}
+	_putchar(10);
+}
