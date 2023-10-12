f4bf199dded2ad0be9843a26446a27234043be6f
MICHAEL MURITHI <michael.muri2020@gmail.com>
2023-10-12 15:21:26 +0300

    new file added

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,20 @@
+int _isdigit(int c);
+/**
+ * _isdigit - checks for a digit (0 through 9).
+ * @c: number to be checked.
+ * Return: 1 if c is a digit else 0.
+ */
+int _digit(int c)
+{
+	int curr = 0;
+	
+	while (curr < 10)
+	{
+		if (c == curr)
+		{
+			return (1);
+		}
+		curr++;
+	}
+	return (0);
+}
