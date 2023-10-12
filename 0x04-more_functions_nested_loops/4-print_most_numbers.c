b2620d02d60df34bd45ea0633edffa919d5994eb
MICHAEL MURITHI <michael.muri2020@gmail.com>
2023-10-12 18:07:49 +0300

    fixed betty coding style

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * print_most-numbers - prints numbers from 0 to 9 except 2 and 4.
+ * print_most_numbers - prints numbers from 0 to 9 except 2 and 4.
  */
 void print_most_numbers(void)
 {
