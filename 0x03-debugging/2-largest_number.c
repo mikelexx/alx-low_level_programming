fe44f5985f89f8311567ad7da4abdc26f9a5e5d4
MICHAEL MURITHI <michael.muri2020@gmail.com>
2023-10-13 18:36:44 +0300

    added more logic

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -12,23 +12,23 @@ int largest_number(int a, int b, int c)
 {
     int largest;
 
-    if (a > b && b > c)
+    if (a > b && (b > c || b == c))
     {
         largest = a;
     }
-    else if (a > c && c > b)
+    else if (a > c && (c > b || c == b))
     {
 	    largest = a;
     }
-    else if (b > c && c > a)
+    else if (b > c && (c > a || c == a))
     {
 	    largest  = b;
     }
-    else if (b > a && a > c)
+    else if (b > a && (a > c || a == c))
     {
         largest = b;
     }
-    else if (c > a && a > b)
+    else if (c > a && (a > b || a == b))
     {
         largest = c;
     }
