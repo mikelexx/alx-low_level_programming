fea4c2db5679851d01cbadfd5dd9d0a98e773d04
MICHAEL MURITHI <michael.muri2020@gmail.com>
2023-10-16 20:21:54 +0300

    newfile

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * *_strcpy - copies the string pointed \
+ *  by src including terminating null byte
+ * @dest:  destination buffer
+ * @src: source buffer
+ * Return: pointer to dest
+ */
+char *_strcpy(char *dest, char *src)
+{
+	char *start = dest;
+
+	while (*src)
+	{
+		*dest = *src;
+		src++;
+		dest++;
+	}
+	return start;
+}
+
