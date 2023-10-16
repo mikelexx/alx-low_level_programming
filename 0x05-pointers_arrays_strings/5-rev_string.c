a15cd8d5436cf6f37f4fc65fa5414d6e760724d9
MICHAEL MURITHI <michael.muri2020@gmail.com>
2023-10-16 13:55:17 +0300

    new file

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * rev_string - reverses string
+ * @s: pointer to string to be reversed
+ */
+void rev_string(char *s)
+{
+	char *start = s;
+	while (*s)
+	{
+		s++;
+	}
+	s--;
+	while (s != start)
+	{
+		char tmp = *start;
+
+		*start = *s;
+		*s = tmp;
+		start++;
+		s--;
+	}
+}
+
