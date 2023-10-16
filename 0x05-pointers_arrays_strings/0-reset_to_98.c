89889c0610e33eb4fc86e0c539ab253bec6f96d1
MICHAEL MURITHI <michael.muri2020@gmail.com>
2023-10-16 09:15:34 +0300

    new folder

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * reset_to_98(int *n) - takes pointer as parammeter  \
+ * and updates the value it points to to 98
+ * @n - pointer to be updated
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
