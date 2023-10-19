a96cd206bd07ce447ad091f593c14e1ab2dd2140
MICHAEL MURITHI <michael.muri2020@gmail.com>
2023-10-19 17:14:03 +0300

    newfile

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,33 @@
+#include "main.h"
+/**
+ * cap_string - capitalizes all words \
+ * of a string
+ * @s: pointer to string to be capitalized
+ * Return: pointer to @s
+ */
+char *cap_string(char *s)
+{
+	char *start = s;
+	while (*s)
+	{
+		if (*s >= 97 && *s <= 122)
+		{
+			char p = *(s - 1);
+			if (p == ','
+					|| p == ';'
+					|| p == '.' || p == '?'
+					|| p =='!' || p == 32
+					|| p == 9 || p == 10 || p == '"'
+				      	|| p == 40
+					|| p == 41
+					|| p == 123 || p == 125)
+
+			{
+
+				*s = *s -32;
+			}
+		}
+		s++;
+	}
+	return (start);
+}
