3aaa4825a47fee96cd06c896cbc05ea18874777d
MICHAEL MURITHI <michael.muri2020@gmail.com>
2023-10-19 16:12:35 +0300

    newfile

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * string_toupper - changes all lowercase \
+ * of a string to uppercase
+ * @n - pointer to string
+ * Return: pointer to lowercase string
+ */
+char *string_toupper(char *s)
+{
+	char *start = s;
+
+	while(*s)
+	{
+		if (*s >= 97 && *s <= 122)
+		{
+			*s = *s - 32;
+		}
+		s++;
+	}
+	return (start);
+}
