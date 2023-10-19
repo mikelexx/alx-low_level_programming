7b5aeafc9c1c850187a88e85a52b330ea75658ad
MICHAEL MURITHI <michael.muri2020@gmail.com>
2023-10-19 18:53:02 +0300

    newfile

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,8 +1,58 @@
 #include "main.h"
 /**
- * leet - encodes string to 1337
- * @s: string to be encoded
- * Return: pointer to that string
+ * leet - encodes sing to '1'337
+ * @s: sing to be encoded
+ * Return: pointer to that sing
  */
 char *leet(char *s)
 {
+	char *start = s;
+
+	while (*s)
+	{
+		if(*s == 'a')
+		{
+			*s = '4';
+		}
+		else if (*s == 'A')
+		{
+			*s = '4';
+		}
+		else if (*s == 'e')
+		{
+			*s = '3';
+		}
+		else if (*s == 'E')
+		{
+			*s = '3';
+		}
+		else if (*s == '0')
+		{
+			*s = '0';
+		}
+		else if (*s == 'o')
+		{
+			*s = '0';
+		}
+		else if (*s == 'l')
+		{
+			*s = '1';
+		}
+		else if (*s == 'L')
+		{
+			*s = '1';
+		}
+		else if (*s == 't')
+		{
+			*s = '7';
+		}
+		else if (*s == 'T')
+		{
+			*s = '7';
+		}
+		s++;
+	}
+	return (start);
+}
+
+
