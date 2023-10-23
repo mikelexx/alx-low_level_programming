fe15f36f3eeb5ce2fddb18fe06a0373eed236b03
MICHAEL MURITHI <michael.muri2020@gmail.com>
2023-10-23 17:28:23 +0300

    changed code logic

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <stddef.h>
 /**
  * _strspn - gets the length of a prefix substring
  * @s: string on which to check prefix substring
@@ -8,3 +7,30 @@
  consist only of bytes from accept
  */
 unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int count = 0;
+	char *start = accept;
+	int flag = 1;
+
+	while (*s)
+	{
+		flag = 1;
+		accept = start;
+		while (*accept)
+		{
+			if (*s == *accept)
+			{
+				count += 1;
+				flag = 0;
+				break;
+			}
+			accept++;
+		}
+		if (flag)
+		{
+			return (count);
+		}
+		s++;
+	}
+	return (count);
+}
