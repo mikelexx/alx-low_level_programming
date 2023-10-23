bb4078ec3fe3ed28ef512d6f08ddd04ce73662d6
MICHAEL MURITHI <michael.muri2020@gmail.com>
2023-10-23 15:52:07 +0300

    fixed code logic

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stddef.h>
 /**
  * _strchr - locates a character in a string
  * @s: pointer to string to operate on
@@ -8,7 +9,7 @@
  */
 char *_strchr(char *s, char c)
 {
-	while (*s != '\0')
+	while (*s)
 	{
 		if (*s == c)
 		{
