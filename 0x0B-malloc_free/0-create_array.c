d8d0b68cd350a12735b2af2b1701f2847abc9037
MICHAEL MURITHI <michael.muri2020@gmail.com>
2023-10-30 19:04:23 +0300

    added condtion to check for null pointer

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -12,7 +12,11 @@ char *create_array(unsigned int size, char c)
 {
 	unsigned int i;
 	char *ptr = (char *) malloc(sizeof(char) * size);
-
+	
+	if (ptr == NULL)
+	{
+		return (NULL);
+	}
 	if (size == 0)
 	{
 		return (NULL);
