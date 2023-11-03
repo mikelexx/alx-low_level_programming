0dd36d9a88f20f27e48b4fc8a20f5cf47870996f
MICHAEL MURITHI <michael.muri2020@gmail.com>
2023-11-03 17:10:15 +0300

    modified

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -8,10 +8,15 @@
  */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	unsigned int x, y, z;
-	char *s;
-	
-	if(s1 == NULL)
-		x = 0;:
+	unsigned int b;
+	char *a;
 
-
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+	a = malloc(nmemb * size);
+	if (a == NULL)
+		return (NULL);
+	for (b = 0; b < (nmemb * size); b++)
+		a[b] = 0;
+	return (a);
+}
