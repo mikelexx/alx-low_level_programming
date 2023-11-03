16aef9893607916f8edc9ae27e8e16909a5d2c0c
MICHAEL MURITHI <michael.muri2020@gmail.com>
2023-11-03 17:31:05 +0300

    modified

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -14,6 +14,8 @@ int *array_range(int min, int max)
 	if (min > max)
 		return (NULL);
 	arr = malloc(sizeof(int) * (max - min + 1));
+	if (arr == NULL)
+		return (NULL);
 	for (i = 0; min <= max; i++)
 		arr[i] = min++;
 	return (arr);
