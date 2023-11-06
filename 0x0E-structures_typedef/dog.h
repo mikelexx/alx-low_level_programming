169685250f98942ad297a35f9692cb2f6e9da8fe
MICHAEL MURITHI <michael.muri2020@gmail.com>
2023-11-06 16:34:22 +0300

    added include guard and fixed betty codin style

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,5 +1,7 @@
+#ifndef DOG_H
+#define DOG_H
 /**
- * struct dog
+ * struct dog  - animal
  * @name: name of dog
  * @owner: its owners name
  * @age: age
@@ -10,3 +12,4 @@ struct dog
 	float age;
 	char *owner;
 };
+#endif
