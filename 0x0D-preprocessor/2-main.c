e4f7116b574eeac67882fa1df913611fb5d20ae9
MICHAEL MURITHI <michael.muri2020@gmail.com>
2023-11-06 10:29:35 +0300

    new file

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,11 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * main - prints name of file it was compiled from
+ * Return: 0 for sucess 1 else
+ */
+int main()
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
