09cd016355c50ade53cd68f8591e6d023d070037
MICHAEL MURITHI <michael.muri2020@gmail.com>
2023-11-06 18:52:14 +0300

    newfile

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,14 @@
+#include "dog.h"
+/**
+ * init_dog - initializes dog struct
+ * @d: pointer to struct dog
+ * @name: pointer to name member of dog struct
+ * @age: pointer to age memebr of dog struct
+ * @owner: pointer to owner memeber of dog struct
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d->name  = name;
+	d->age = age;
+	d->owner = owner;
+}
