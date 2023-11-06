e13bf5bd26ff831e1f1ec7aaea24045245e3bcef
MICHAEL MURITHI <michael.muri2020@gmail.com>
2023-11-06 19:31:54 +0300

    changed name for struct dog to dog_t

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -6,12 +6,12 @@
  * @owner: its owners name
  * @age: age
  */
-struct dog
+typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-};
+}dog_t;
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 #endif
