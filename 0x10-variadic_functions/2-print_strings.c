ea51427ead122b0c9b6ad31c57dbea6a563d9252
MICHAEL MURITHI <michael.muri2020@gmail.com>
2023-11-09 18:41:01 +0300

    changed code logic so as to cate for zero variable arguments (n == 0)

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -15,20 +15,21 @@ void print_strings(const char *separator, const unsigned int n, ...)
 	char *s;
 
 	va_start(args, n);
-	for (i = 0; i < n - 1; i++)
+	for (i = 0; i < n; i++)
 	{
-		s = va_arg(args, char *);
-		if (s != NULL)
-			printf("%s", s);
+		if (i == 0)
+			printf("%s", va_arg(args, char *));
 		else
-			printf("(nil)\n");
-		if (separator != NULL)
-			printf("%s", separator);
+		{
+			s = va_arg(args, char *);
+			if (separator != NULL)
+				printf("%s", separator);
+			if (s)
+				printf("%s", s);
+			else
+				printf("(nil)");
+		}
 	}
-	s = va_arg(args, char *);
-	if (s != NULL)
-		printf("%s\n", s);
-	else
-		printf("(nil)\n");
 	va_end(args);
+	printf("\n");
 }
