eeadc4bf54337fc5b941e0a2efd3fbd667ee6e1d
MICHAEL MURITHI <michael.muri2020@gmail.com>
2023-11-16 16:30:31 +0300

    fixed betty coding style

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,7 +1,7 @@
 #include "lists.h"
 /**
  * list_len - returns number of elements in a linked lis_t list
- * @h: head of linked list 
+ * @h: head of linked list
  * Return: number of elements
  */
 size_t list_len(const list_t *h)
@@ -9,7 +9,7 @@ size_t list_len(const list_t *h)
 	size_t count = 0;
 	const list_t *tmp = h;
 
-	while(tmp)
+	while (tmp)
 	{
 		tmp = tmp->next;
 		count++;
