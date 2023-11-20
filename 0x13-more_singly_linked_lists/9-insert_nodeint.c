659f35e0aa6f669bb4522cdd32a003926f33199f
MICHAEL MURITHI <michael.muri2020@gmail.com>
2023-11-20 21:39:07 +0300

    fixed typo h instead of left

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -30,7 +30,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	}
 	h = (*head)->next;
 	left = h;
-	while (h && i <= idx)
+	while (left && i <= idx)
 	{
 		if (i == idx)
 		{
