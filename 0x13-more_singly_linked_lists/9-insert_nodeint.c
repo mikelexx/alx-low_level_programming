1ea2502387adf31f9c47317e1389f019ffa4edce
MICHAEL MURITHI <michael.muri2020@gmail.com>
2023-11-20 21:43:22 +0300

    made the newnode the pointer-only check 6 was wrong b4 this commit

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -25,7 +25,6 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	if (idx == 0)
 	{
 		newnode->next = *head;
-		*head = newnode;
 		return (newnode);
 	}
 	h = (*head)->next;
