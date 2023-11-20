0d9110936326e7abd14de62493c13c230084c2c1
MICHAEL MURITHI <michael.muri2020@gmail.com>
2023-11-20 23:31:04 +0300

    modified checking for head:

--- 0x13-more_singly_linked_lists/103-find_loop.c
+++ 0x13-more_singly_linked_lists/103-find_loop.c
@@ -9,7 +9,7 @@ listint_t *find_listint_loop(listint_t *head)
 {
 	listint_t *fast, *slow;
 
-	if (head == NULL)
+	if (head == NULL || *head == NULL)
 		return (NULL);
 	fast = head, slow = head;
 	while (fast && fast->next)
