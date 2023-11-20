d6ea5a729e29e10f9fc6c9a7dfef80678f3c506a
MICHAEL MURITHI <michael.muri2020@gmail.com>
2023-11-20 21:55:43 +0300

    fixed proper way fo replacing head with newnode

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -9,7 +9,7 @@
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
 	unsigned int i = 1;
-	listint_t  *h, *newnode, *left;
+	listint_t  *h, *newnode, *left, *oldh;
 
 	if (head == NULL)
 		return (NULL);
@@ -24,7 +24,9 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	}
 	if (idx == 0)
 	{
+		oldh = *head;
 		*head = newnode;
+		newnode->next = oldh;
 		return (newnode);
 	}
 	h = (*head)->next;
