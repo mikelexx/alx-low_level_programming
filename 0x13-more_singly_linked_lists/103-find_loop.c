42c0ba948bc93bc77abc6a9389c087adbfc0ee0a
MICHAEL MURITHI <michael.muri2020@gmail.com>
2023-11-20 23:39:36 +0300

    modified

--- 0x13-more_singly_linked_lists/103-find_loop.c
+++ 0x13-more_singly_linked_lists/103-find_loop.c
@@ -9,7 +9,7 @@ listint_t *find_listint_loop(listint_t *head)
 {
 	listint_t *fast, *slow;
 
-	if (head == NULL || *head == NULL)
+	if (head == NULL)
 		return (NULL);
 	fast = head, slow = head;
 	while (fast && fast->next)
