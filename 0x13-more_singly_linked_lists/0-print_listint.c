88970cefe4f683b768b38d5b3cf12508d1ee8ac0
MICHAEL MURITHI <michael.muri2020@gmail.com>
2023-11-20 16:50:56 +0300

    initial commit

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+/**
+ * print_listint - prints all elements of a lisint_t list
+ * @h: head of the list
+ * Return: number of nodes
+ */
+size_t print_listint(const listint_t *h)
+{
+	size_t count = 0;
+
+	while (h)
+	{
+		printf("%d\n",h->n);
+		count++;
+		h = h->next;
+	}
+	return (count);
+}
+
