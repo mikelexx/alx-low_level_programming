c95af8a55b61dd3c0ba7fe51eea82fe7586e3e1a
MICHAEL MURITHI <michael.muri2020@gmail.com>
2023-11-23 18:57:27 +0300

    added condition to check for outof range index

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -7,7 +7,7 @@
  */
 int clear_bit(unsigned long int *n, unsigned int index)
 {
-	if (n == NULL)
+	if (index >= sizeof(unsigned long int) * 8)
 		return (-1);
 	*n =  (*n) & (~(1 << index));
 	return (1);
