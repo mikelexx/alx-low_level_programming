dbf4cb7ff39e4c74fd1f26bb95e4a134a2437195
MICHAEL MURITHI <michael.muri2020@gmail.com>
2023-11-23 19:34:02 +0300

    easy way to print binary representation of number

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -5,33 +5,7 @@
  */
 void print_binary(unsigned long int n)
 {
-	unsigned long int  curr = 1, one = 1, zero = 0;
-
-	if (n == one)
-	{
-		_putchar('1');
-		return;
-	}
-	if (n == zero)
-	{
-		_putchar ('0');
-		return;
-	}
-	while (curr <= n)
-	{
-		curr = curr << 1;
-	}
-	curr = curr >> 1;
-	while (curr > 0)
-	{
-		if (n >= curr)
-		{
-			_putchar('1');
-			n = n - curr;
-		}
-		else
-			_putchar('0');
-		curr  =  curr >> 1;
-	}
+	if (n > 1)
+		print_binary(n >> 1);
+	_putchar((n & 1) + 48);
 }
-
