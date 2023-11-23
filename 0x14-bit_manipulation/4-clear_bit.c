cb0c3532578c05a76157644da0dc382aac72d5f3
MICHAEL MURITHI <michael.muri2020@gmail.com>
2023-11-23 18:38:10 +0300

    momodified

---
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * clear_bit- clears bit at certain index
+ * @n: number of which to clear bit from
+ * @index: index of which to clear that bit from
+ * Return: 1 for success else -1 for any error
+ */
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+	if (n == NULL)
+		return (-1);
+	*n =  (*n) ^ (1 << index);
+	return (1);
+}
