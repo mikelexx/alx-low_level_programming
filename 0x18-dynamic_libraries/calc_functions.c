24499aded9ad52cc39841079f963313d2cb9399a
MICHAEL MURITHI <michael.muri2020@gmail.com>
2024-01-15 10:39:15 +0300

    [new]:Let's call C functions from Python

---
+++ 0x18-dynamic_libraries/calc_functions.c
@@ -0,0 +1,31 @@
+# include "cfunctions.h"
+int add(int a, int b)
+{
+	int c = a + b;
+
+	return (c);
+}
+int sub(int a, int b)
+{
+	int d = a  - b;
+
+	return (d);
+}
+int mul(int a, int b)
+{
+	int e =  a * b;
+
+	return (e);
+}
+int div(int a, int b)
+{
+	int f =  a / b;
+
+	return (f);
+}
+int mod(int a, int b)
+{
+	int g =  a % b;
+
+	return (g);
+}
---
+++ 0x18-dynamic_libraries/cfunctions.h
@@ -0,0 +1,8 @@
+#ifndef _CFUNCTIONS_H
+#define _CFUNCTIONS_H
+int add(int a, int b); 
+int sub(int a, int b); 
+int mul(int a, int b); 
+int div(int a, int b); 
+int mod(int a, int b); 
+#endif 
