c58d533217f567d4831c608722bac0c91b593ac0
MICHAEL MURITHI <michael.muri2020@gmail.com>
2024-02-01 23:25:41 +0300

    5. >>> print(ht)

---
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -0,0 +1,37 @@
+#include "hash_tables.h"
+/**
+ * hash_table_print - prints a hash table.
+ * @ht: the hash table to be printed.
+ * Description: You should print the key/value in the \
+ * order that they appear in the array of hashtable.
+ * Order: array, list.
+ */
+void hash_table_print(const hash_table_t *ht)
+{
+	unsigned long int i;
+	hash_node_t *curr = NULL, *prev = NULL;
+
+	printf("{");
+	for (i = 0; i < ht->size; i++)
+	{
+		curr = (ht->array)[i];
+
+		if (curr)
+		{
+			if (prev)
+				printf("'%s': '%s', ", prev->key, prev->value);
+			while (curr->next)
+			{
+				printf("'%s': '%s', ", curr->key, curr->value);
+				curr = curr->next;
+			}
+			prev = curr;
+
+		}
+
+	}
+	if (prev)
+		printf("'%s': '%s'", prev->key, prev->value);
+
+	printf("}\n");
+}
