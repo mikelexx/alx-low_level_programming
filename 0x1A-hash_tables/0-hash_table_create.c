4a85e61dbf98ee9972e233d4ed298a0c7a0bb406
MICHAEL MURITHI <michael.muri2020@gmail.com>
2024-02-02 16:55:14 +0300

    add check for size  = 0

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -9,7 +9,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 	unsigned long int i;
 	hash_table_t *table = malloc(sizeof(hash_table_t));
 
-	if (table == NULL)
+	if (table == NULL || size == 0)
 		return (NULL);
 	table->array = (hash_node_t **)malloc(size * sizeof(hash_node_t));
 	if (table->array == NULL)
