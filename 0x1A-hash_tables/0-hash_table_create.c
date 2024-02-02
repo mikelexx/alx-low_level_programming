503e90d1e67a0f968b0faa03ebcecd09a856907d
MICHAEL MURITHI <michael.muri2020@gmail.com>
2024-02-02 16:58:47 +0300

    free hashtable before returning is size is 0

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -10,7 +10,10 @@ hash_table_t *hash_table_create(unsigned long int size)
 	hash_table_t *table = malloc(sizeof(hash_table_t));
 
 	if (table == NULL || size == 0)
+	{
+		free(table);
 		return (NULL);
+	}
 	table->array = (hash_node_t **)malloc(size * sizeof(hash_node_t));
 	if (table->array == NULL)
 	{
