68ac3e115eebc4ccd15f225cad545044b815928d
MICHAEL MURITHI <michael.muri2020@gmail.com>
2024-05-08 07:10:41 +0300

    check for empty list

--- 0x1E-search_algorithms/102-interpolation.c
+++ 0x1E-search_algorithms/102-interpolation.c
@@ -13,7 +13,7 @@ int interpolation_search(int *array, size_t size, int value)
 {
 	size_t high = size - 1, low = 0, pos;
 
-	if (array == NULL)
+	if (array == NULL || size == 0)
 		return (-1);
 
 	while (low <= high && array[low] <= value && array[high] >= value)
